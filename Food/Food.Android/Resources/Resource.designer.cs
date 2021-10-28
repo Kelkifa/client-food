924a1dbcf016d2f7bff58b927f05c396372e9b12
Pham Long Huan <huanvip455@gmail.com>
2021-10-28 21:18:31 +0700

    fix image slider

--- Food/Food.Android/Resources/Resource.designer.cs
+++ Food/Food.Android/Resources/Resource.designer.cs
@@ -15021,19 +15021,34 @@ namespace Food.Droid
 			public const int Prev = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int test_custom_background = 2131165333;
+			public const int slide1 = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int tooltip_frame_dark = 2131165334;
+			public const int slide2 = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int tooltip_frame_light = 2131165335;
+			public const int slide3 = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int Up = 2131165336;
+			public const int slide4 = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int watermark = 2131165337;
+			public const int slide5 = 2131165337;
+			
+			// aapt resource value: 0x7F07009A
+			public const int test_custom_background = 2131165338;
+			
+			// aapt resource value: 0x7F07009B
+			public const int tooltip_frame_dark = 2131165339;
+			
+			// aapt resource value: 0x7F07009C
+			public const int tooltip_frame_light = 2131165340;
+			
+			// aapt resource value: 0x7F07009D
+			public const int Up = 2131165341;
+			
+			// aapt resource value: 0x7F07009E
+			public const int watermark = 2131165342;
 			
 			static Drawable()
 			{

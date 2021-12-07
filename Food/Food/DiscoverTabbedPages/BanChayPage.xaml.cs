beb80505e9187cbad5fbd0f582b4cd51efb8b836
Pham Long Huan <huanvip455@gmail.com>
2021-12-07 17:31:44 +0700

    ban chay convert food cost

--- Food/Food/DiscoverTabbedPages/BanChayPage.xaml.cs
+++ Food/Food/DiscoverTabbedPages/BanChayPage.xaml.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Food.DiscoverTabbedPages
 {
+
     public class TestData
     {
         public string image { get; set; }
@@ -25,19 +29,18 @@ namespace Food.DiscoverTabbedPages
             InitialData();
         }
 
-        void InitialData()
+        async void InitialData()
         {
-            List<TestData> testData = new List<TestData>();
-            testData.Add(new TestData() { image = "https://media-cdn.laodong.vn/Storage/NewsPortal/2021/4/6/896286/Qua-Tao-1.jpg", text = "Test data" });
-            testData.Add(new TestData() { image = "https://i.gadgets360cdn.com/products/large/micromax-in-2b-1-414x800-1627626668.jpg?downsize=240:*&output-quality=80&output-format=webp", text = "Test data 2" });
-            testData.Add(new TestData() { image = "https://media-cdn.laodong.vn/Storage/NewsPortal/2021/4/6/896286/Qua-Tao-1.jpg", text = "Test data 2" });
-            testData.Add(new TestData() { image = "https://media-cdn.laodong.vn/Storage/NewsPortal/2021/4/6/896286/Qua-Tao-1.jpg", text = "Test data 2" });
-            testData.Add(new TestData() { image = "https://media-cdn.laodong.vn/Storage/NewsPortal/2021/4/6/896286/Qua-Tao-1.jpg", text = "Test data 2" });
-            testData.Add(new TestData() { image = "https://media-cdn.laodong.vn/Storage/NewsPortal/2021/4/6/896286/Qua-Tao-1.jpg", text = "Test data 2" });
+            HttpClient http = new HttpClient();
+
+            string response = await http.GetStringAsync("https://xamarin-food.herokuapp.com/api/food/json");
+
+            List<Food> foodList = JsonConvert.DeserializeObject<List<Food>>(response);
 
 
+            //DisplayAlert("Thong bao", response, "OK");
 
-            lstBanChay.ItemsSource = testData;
+            lstBanChay.ItemsSource = foodList;
         }
 
         private void lstBanChay_SelectionChanged(object sender, SelectionChangedEventArgs e)
---
+++ Food/Food/Food.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Food
+{
+    public class Food
+    {   
+        public string _id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string image { get; set; }
+        public string description { get; set; }
+        public string production { get; set; }
+        public int cost { get; set; }
+        public string unit { get; set; }
+        public string minMass { get; set; }
+        public string maxMass { get; set; }
+    }
+}

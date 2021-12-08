dcff06db03fa9f1e6a635b8ff8e71ae49d4794aa
DucTran2310 <18520021@gm.uit.edu.vn>
2021-12-08 15:16:29 +0700

    Add xem nhanh page

---
+++ Food/Food/DiscoverTabbedPages/XemNhanhPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Food.DiscoverTabbedPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XemNhanhPage : ContentPage
+    {
+        public XemNhanhPage()
+        {
+            InitializeComponent();
+            CreateXemNhanh();
+        }
+
+        List<XemNhanh> xemNhanhList = new List<XemNhanh>();
+
+        void CreateXemNhanh()
+        {
+
+
+            xemNhanhList.Add(new XemNhanh
+                {
+                    XemNhanhID = "XN1",
+                    XemNhanhLabel = "Mã Ưu Đãi",
+                    XemNhanhImage = "MUD.jpg"
+                });
+
+
+            xemNhanhList.Add(new XemNhanh
+                {
+                    XemNhanhID = "XN2",
+                    XemNhanhLabel = "Chopp Ưu Đãi",
+                    XemNhanhImage = "ChopUD.jpg"
+                });
+
+            xemNhanhList.Add(new XemNhanh
+                {
+                    XemNhanhID = "XN3",
+                    XemNhanhLabel = "Mới Mới Mới",
+                    XemNhanhImage = "Moi.jpg"
+                });
+
+            xemNhanhList.Add(new XemNhanh
+                {
+                    XemNhanhID = "XN4",
+                    XemNhanhLabel = "Công thức",
+                    XemNhanhImage = "ChopCT.jpg"
+                });
+            xemNhanhList.Add(new XemNhanh
+                {
+                    XemNhanhID = "XN5",
+                    XemNhanhLabel = "Chopp Bật Mí",
+                    XemNhanhImage = "ChopBM.jpg"
+                });
+            xemNhanhList.Add(new XemNhanh
+                {
+                    XemNhanhID = "XN6",
+                    XemNhanhLabel = "Chopp VIP",
+                    XemNhanhImage = "ChopVIP.jpg"
+                });
+            
+
+            XemNgay.ItemsSource = xemNhanhList;
+        }
+    }
+}
---
+++ Food/Food/XemNhanh.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Food
+{
+    public class XemNhanh
+    {
+        public string XemNhanhID { get; set; }
+        public string XemNhanhLabel { get; set; }
+        public string XemNhanhImage { get; set; }
+    }
+}

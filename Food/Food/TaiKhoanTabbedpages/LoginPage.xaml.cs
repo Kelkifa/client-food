aa7a9739f0217d0d207f5b0406c50a9a750f568c
DucTran2310 <18520021@gm.uit.edu.vn>
2021-12-10 10:05:02 +0700

    Add loginpage, register page

---
+++ Food/Food/TaiKhoanTabbedpages/LoginPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Food.TaiKhoanTabbedpages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogIn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Register_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
---
+++ Food/Food/TaiKhoanTabbedpages/RegisterPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Food.TaiKhoanTabbedpages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+    }
+}

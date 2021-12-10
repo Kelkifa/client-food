2d1eca45f8ba430977050603b682fadd57aa4b1f
Pham Long Huan <huanvip455@gmail.com>
2021-12-10 17:42:22 +0700

    finish

---
+++ Food/Food/Auth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Food
+{
+    public class Auth
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public static string currUsername = null;
+    }
+}

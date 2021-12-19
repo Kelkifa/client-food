3138a77de95f2a9a91b4f9aa6596b65fe9d410b2
Huân Phạm Long <18520788@ms.uit.edu.vn>
2021-12-19 20:56:56 +0700

    order class fix

--- Food/Food/Order.cs
+++ Food/Food/Order.cs
@@ -6,12 +6,13 @@ namespace Food
 {
     public class Order
     {
-        public Auth user {get;set;}
+        public User user {get;set;}
         public List<Cart> cartList { get; set; }
 
         public string address { get; set; }
         public string sdt { get; set; }
 
+        public bool isAccept { get; set; }
         public string updatedAt { get; set; }
         public string createdAt { get; set; }
     }

070c100d6461c86bf0d091e7f33f253a32a28e53
minhsuu14 <minhsuu14@gmail.com>
2021-12-20 15:15:23 +0700

    ChiTietHoaDonPage added

---
+++ Food/Food/ChiTietHoaDonPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Food
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChiTietHoaDonPage : ContentPage
+    {
+        List<Cart> carts = null;
+        User user = null;
+        Order order;
+        public ChiTietHoaDonPage()
+        {
+            InitializeComponent();
+        }
+        public ChiTietHoaDonPage(List<Cart> cartList, User usr)
+        {
+            carts = cartList;
+            user = usr;
+            InitializeComponent();
+        }
+        void InitData()
+        {
+            order.cartList = carts;
+            order.user = user;
+        }
+    }
+}

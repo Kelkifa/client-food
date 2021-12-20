566267c19f1cd5bac31231d380b6d3248a69e506
Huân Phạm Long <18520788@ms.uit.edu.vn>
2021-12-20 11:07:14 +0700

    CartPage thêm thông báo nếu giỏ hàng trống và nút xóa all

--- Food/Food/CartPage.xaml.cs
+++ Food/Food/CartPage.xaml.cs
@@ -20,7 +20,6 @@ namespace Food
         public CartPage()
         {
             InitializeComponent();
-            InitFoodList();
         }
 
         public CartPage(bool isReload)
@@ -100,9 +99,24 @@ namespace Food
 
         }
 
+        private void InitNotifice()
+        {
+            if(this.cartList != null)
+            {
+                if(this.cartList.Count > 0)
+                {
+                    txtEmptyNotifice.IsVisible = false;
+                    lstCart.IsVisible = true;
+                    return;
+                }
+            }
+                txtEmptyNotifice.IsVisible = true;
+                lstCart.IsVisible = false;
+        }
+
         private int CaculateCost(int cost, int discount, int soLuong)
         {
-            return (cost - discount * cost /100) * soLuong;
+            return (cost - discount * cost / 100) * soLuong;
         }
 
         private void thanhToanBtn_Clicked(object sender, EventArgs e)
@@ -138,6 +152,47 @@ namespace Food
             this.totalCost = tongTien;
 
             amount.Text = "Tổng tiền: " + ConvertCost(this.totalCost);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            InitFoodList();
+            InitNotifice();
+        }
+
+        private async void btnDelAll_Clicked(object sender, EventArgs e)
+        {
+            List<String> selectedCartId = new List<string>();
+
+            foreach (Cart cart in this.cartList)
+            {
+                selectedCartId.Add(cart._id);
+            }
+
+
+            if (selectedCartId.Count != 0)
+            {
+                ApiCall api = new ApiCall();
+
+                CartResponse apiResponse = await api.fetchRemoveCartListAsync(selectedCartId);
+
+                if (apiResponse.success)
+                {
+                    _ = DisplayAlert("Thông báo", "Xóa thành công", "OK");
+                    this.cartList = null;
+                    lstCart.ItemsSource = null;
+                    InitFoodList();
+                }
+                else
+                {
+                    _ = DisplayAlert("Thông báo", apiResponse.message, "OK");
+                }
+            }
+            else
+            {
+                _ = DisplayAlert("Thông báo", "Không có sản phẩm nào để xoá", "OK");
+            }
         }
     }
 }

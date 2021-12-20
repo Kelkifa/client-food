9cbd52e603f98acc9b99096a728024e19237f6c1
minhsuu14 <minhsuu14@gmail.com>
2021-12-20 20:14:55 +0700

    ChiTietHoaDon new added

--- Food/Food/ChiTietHoaDonPage.xaml.cs
+++ Food/Food/ChiTietHoaDonPage.xaml.cs
@@ -12,23 +12,58 @@ namespace Food
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ChiTietHoaDonPage : ContentPage
     {
-        List<Cart> carts = null;
-        User user = null;
         Order order;
         public ChiTietHoaDonPage()
         {
             InitializeComponent();
+            InitData();
         }
-        public ChiTietHoaDonPage(List<Cart> cartList, User usr)
+        public ChiTietHoaDonPage(Order ord)
         {
-            carts = cartList;
-            user = usr;
+            order = ord;
             InitializeComponent();
+            InitData();
         }
         void InitData()
         {
-            order.cartList = carts;
-            order.user = user;
+            txtAddr.Text = order.address;
+            txtDate.Text = order.createdAt;
+            txtName.Text = order.user.fullname;
+            txtPhone.Text = order.user.sdt;
+            txtBillId.Text = "Mã Đơn: " + order._id;
+            txtTotalCost.Text = ConvertCost(TotalCostFunc(true));
+            txtTotalCost1.Text = ConvertCost(TotalCostFunc(false));
+            
+            txtTotalDiscount.Text = "-" + ConvertCost(TotalDiscountFunc());
+            collectionData.ItemsSource = order.cartList;
+        }
+        int TotalCostFunc(bool check)
+        {
+            int totalCost = 0;
+            if (order.cartList != null && check == true) 
+            {
+                //totalCost with discount
+                totalCost = order.cartList.Sum(x => (x.food.cost - (x.food.cost * x.food.discount / 100)) * x.soLuong);
+            }
+            else if (order.cartList != null && check == false)
+            {
+                //totalCost without discount
+                totalCost = order.cartList.Sum(x => x.food.cost * x.soLuong);
+            }
+            return totalCost;
+        }
+        int TotalDiscountFunc()
+        {
+            int total = 0;
+            if (order.cartList != null)
+            {
+                total = order.cartList.Sum(x => x.food.cost * x.food.discount / 100  * x.soLuong);
+            }
+            return total;
+        }
+        string ConvertCost(int cost)
+        {
+            return cost.ToString("N0") + "đ";
         }
     }
 }

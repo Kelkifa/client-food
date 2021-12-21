f3176ad02e27efbefd9bc8d084a4e39089a52612
minhsuu14 <minhsuu14@gmail.com>
2021-12-21 18:25:35 +0700

    DSHoaDonPage added

---
+++ Food/Food/DSHoaDonPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Food.ApiResponseClass;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Food
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DSHoaDonPage : ContentPage
+    {
+        public DSHoaDonPage()
+        {
+            InitializeComponent();
+            GetMyOrders();
+        }
+        private async void GetMyOrders() 
+        {
+            ApiCall api = new ApiCall();
+            OrderResponse orderResponse = await api.fetchGetOrderAsync();
+            if(orderResponse.success)
+            {
+                lstOrder.ItemsSource = orderResponse.response;
+            }
+        }
+
+        private void lstOrder_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Order order = new Order();
+            order = (Order)lstOrder.SelectedItem;
+            Navigation.PushAsync(new ChiTietHoaDonPage(order));
+        }
+    }
+}
